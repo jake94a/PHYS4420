9803126b9da550231db080c14fd065fc1a95c859
FoundedSine14 <54042868+jake94a@users.noreply.github.com>
2020-03-02 22:11:43 -0700

    added descriptive comments

--- PHYS4420_HW6.py
+++ PHYS4420_HW6.py
@@ -4,8 +4,12 @@ Author: Jake R. Anderson
 Professor: Dr. Steve Wasserbaech
 
 Description:
-A) Plot the phase velocity and group velocity as functions of wavelength covering the wavelength range from 10**-4 to
-10**2 meters. Use log scales on both axes. Plot velocity vs lambda (y vs x).
+A) Plot the Phase Velocity and Group Velocity as functions of Wavelength covering the Wavelength range from 10**-4 to
+10**2 meters. Use log scales on both axes. Plot Phase Velocity vs Wavelength and plot Group Velocity vs Wavelength
+individually.
+
+B) Find the wavelength at which the Phase and Group velocities are equal. Plot both Phase and Group Velocities vs 
+Wavelength on the same figure and find their intersection point.
 
 Function:
 A) Given an equation using
@@ -14,11 +18,16 @@ A) Given an equation using
 . wave_number = unknown
 . surface_tension = 0.0720
 We are given a range of "lambda" values
-Use the relation "lambda = (2 * Pi) / wave_num" to find list of wave_num values
+Use the relation "lambda = (2 * pi) / wave_num" to find list of wave_num values
 Now use relations:
 1) Phase Velocity = Frequency / wave_num
 2) Group Velocity = d(Frequency) / d(wave_num)
 to find (1) and (2) vs lambda
+Plot (1) vs Wavelength
+Plot (2) vs Wavelength
+
+B)
+Plot both (1) and (2) vs Wavelength on the same figure
 
 """
 import numpy as np
@@ -55,16 +64,19 @@ def group_velocity(gravity, surface_tension, density, wave_number_list):
     return group_velocity_list
 
 
+# Define constants
 sur_tension_a = 0.0720  # kg / s ** 2
 grav_a = 9.8  # m / s ** 2
 density_water_a = 1000  # kg / m ** 3
 wavelength_list_a = np.logspace(-4, 2)
 
+# Find wave number values then use them to find omega, phase, and group velocity
 wave_numbers_a = wave_number(wavelength_list_a)
 water_waves_omega_a = deep_water_waves(grav_a, sur_tension_a, density_water_a, wave_numbers_a)
 phase_velocity_a = phase_velocity(water_waves_omega_a, wave_numbers_a)
 group_velocity_a = group_velocity(grav_a, sur_tension_a, density_water_a, wave_numbers_a)
 
+# Plot phase velocity vs wavelength
 plt.figure(1)
 plt.title('HW6(A.1) Phase Velocity vs Wavelength')
 plt.xscale('log')
@@ -73,6 +85,7 @@ plt.plot(wavelength_list_a, phase_velocity_a)
 plt.xlabel('wavelength (m)')
 plt.ylabel('phase velocity')
 
+# Plot group velocity vs wavelength
 plt.figure(2)
 plt.title('HW6(A.2) Group Velocity vs Wavelength')
 plt.xscale('log')
@@ -82,7 +95,8 @@ plt.xlabel('wavelength (m)')
 plt.ylabel('group velocity')
 plt.show()
 
-plt.title('HW6(B) Group Velocity and Phase Velocity vs Wavelength')
+# Plot both phase and group velocities vs wavelength
+plt.title('HW6(B) Phase Velocity and Group Velocity vs Wavelength')
 plt.xscale('log')
 plt.yscale('log')
 plt.plot(wavelength_list_a, phase_velocity_a)
